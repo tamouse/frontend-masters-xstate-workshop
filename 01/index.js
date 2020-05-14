351e058aaa88d7072ec2d836fd4610fbef439256
Tamara Temple <tamouse@gmail.com>
2020-05-14 11:36:26 -0500

    trying to follow along
    parcel is not working

--- 01/index.js
+++ 01/index.js
@@ -3,11 +3,29 @@ const elBox = document.querySelector('#box');
 // Pure function that returns the next state,
 // given the current state and sent event
 function transition(state, event) {
-  switch (
+  /*switch (
     state
     // Add your state/event transitions here
     // to determine and return the next state
   ) {
+  }*/
+  switch (state) {
+    case "inactive":
+      switch (event) {
+        case "CLICK":
+          return "active";
+        default:
+          return state;
+      };
+    case "active":
+      switch (event) {
+        case "CLICK":
+          return "inactive";
+        default:
+          return state;
+      };
+    default:
+      return state;
   }
 }
 
@@ -16,10 +34,13 @@ let currentState = undefined;
 
 function send(event) {
   // Determine the next value of `currentState`
-
+  currentState = transition(currentState, event)
+  console.log(currentState)
   elBox.dataset.state = currentState;
 }
 
 elBox.addEventListener('click', () => {
+  console.log("clicked...")
   // send a click event
+  send("CLICK")
 });
--- 04/index.js
+++ 04/index.js
@@ -1,48 +1,50 @@
-import { createMachine, interpret } from 'xstate';
+import { createMachine, interpret } from "xstate"
 
-const elBox = document.querySelector('#box');
+const elBox = document.querySelector("#box")
 
 const setPoint = (context, event) => {
   // Set the data-point attribute of `elBox`
   // ...
-};
+  elBox.dataset.point = `(${event.clientX}, ${event.clientY})`
+}
 
 const machine = createMachine({
-  initial: 'idle',
+  initial: "idle",
   states: {
     idle: {
       on: {
         mousedown: {
           // Add your action here
           // ...
-          target: 'dragging',
+          actions: [setPoint],
+          target: "dragging",
         },
       },
     },
     dragging: {
       on: {
         mouseup: {
-          target: 'idle',
+          target: "idle",
         },
       },
     },
   },
-});
+})
 
-const service = interpret(machine);
+const service = interpret(machine)
 
-service.onTransition((state) => {
-  console.log(state);
+service.onTransition(state => {
+  console.log(state)
 
-  elBox.dataset.state = state.value;
-});
+  elBox.dataset.state = state.value
+})
 
-service.start();
+service.start()
 
-elBox.addEventListener('mousedown', (event) => {
-  service.send(event);
-});
+elBox.addEventListener("mousedown", event => {
+  service.send(event)
+})
 
-elBox.addEventListener('mouseup', (event) => {
-  service.send(event);
-});
+elBox.addEventListener("mouseup", event => {
+  service.send(event)
+})
